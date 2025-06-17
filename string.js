6afb968cd1fc77db0c5af4d72b786dd5ba2d28ec
chotung <ctung@continual.ai>
2025-06-17 16:19:00 -0400

    updated comments on string method

--- string.js
+++ string.js
@@ -1,11 +1,11 @@
-String.prototype.endsWith = function(search, position) {
+String.prototype.endsWith = function (search, position) {
   // normalized input & split string
   const normalizedStr = search.toString()
-  if(position) {
-    const substring = this.slice(0,position)
+  if (position) {
+    const substring = this.slice(0, position)
     return substring.includes(normalizedStr)
   }
-  // runtime o(n) as we go through the length of the array 
-  // space is o(1) does not grow with our input size
+  // runtime o(n) from the includes call
+  // space is o(1) or o(k) for the size of the substring created 
   return this.includes(normalizedStr)
 }
